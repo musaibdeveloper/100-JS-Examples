72e227e85a0f717bc77a90caf0a4d0ac2983d9e2
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-23 22:42:32 +0000

    Files are added

---
+++ 02.js
@@ -0,0 +1,15 @@
+// JavaScript Program to Find the Square Root.
+
+let num = 5;
+
+console.log(num); // 5 
+
+// To find the square root of the number is 
+
+console.log(Math.sqrt(num)); //2.23606797749979
+// It will give the result in points values. 
+
+// Inorder to get the whole number we use Math.floor
+
+console.log(Math.floor(Math.sqrt(num))); // 2
+
