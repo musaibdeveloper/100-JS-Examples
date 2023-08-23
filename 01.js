2cf09e6199486c7fd245a37fb0ca4c0c85c769ef
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-23 03:13:04 +0000

    File is added

---
+++ 01.js
@@ -0,0 +1,15 @@
+// JavaScript Program to Swap Two Variables
+// 01. JavaScript Program to Swap Two Variables
+
+let name1 = "Syed";
+
+let name2 = "musaib";
+
+let temp = name1;
+
+name1 = name2;
+
+name2 =temp
+
+console.log(`The value of name1 after swapping: ${name1}`);
+console.log(`The value of name2 after swapping: ${name2}`);
