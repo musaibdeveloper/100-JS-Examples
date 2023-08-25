c1083b6be1e40f6a3afd4feb38678e9a2a9355a0
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-25 18:53:30 +0000

    Files are Updated

--- 10.js
+++ 10.js
@@ -1,2 +1,16 @@
 // 10. JavaScript Program to Check given number is Prime Number.
-// A prime number is a positive integer that is only divisible by 1 and itself. For example, 2, 3, 5, 7, 11 are the first few prime numbers.
+// A prime number is a positive integer that is only divisible by 1 and itself. 
+// a number that can be divided exactly only by itself and 1, for example 7, 17 and 41.
+
+
+let num = +prompt("Enter the number : ")
+
+if(num === 1){
+    console.log("1 is neither prime nor composite number");
+}
+else if(num > 1){
+    console.log("Number is prime");
+}
+else {
+    console.log("number os not prime");
+}
